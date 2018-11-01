b5f8631116a4db8f435f417650e72ebaf77b9294
Spencer Croy <spencerccroy@gmail.com>
2018-11-01 18:20:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,3 +12,12 @@ def current_player(board)
     "O"
   end
 end
+
+def turn_counter(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+end
